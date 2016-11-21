f7ae55c6a0e62acfd0c2ca55a7006452ad145426
gdmec07150826 <904556948@qq.com>
2016-11-21 13:15:25 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150826/mediaplayerservice/PlayService.java
@@ -0,0 +1,39 @@
+package cn.edu.gdmec.s07150826.mediaplayerservice;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import cn.edu.gdmec.s07150808.mediaplayerservice.R;
+
+public class PlayService extends Service {
+    MediaPlayer mediaPlayer;
+    public PlayService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer=MediaPlayer.create(this, R.raw.exodus);
+        //重播
+        mediaPlayer.setLooping(true);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
